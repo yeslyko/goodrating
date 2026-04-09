yeslyko/goodrating

commit 8aa0496c1ec27d18a2380a39f0553131f32e0fcf

refactor: simplify lookup

goodrating.cpp
@@ -298,9 +298,8 @@ static bool chartReader(const std::string& filename, const std::string& table) {
 				playerTable.emplace(pid, player);
 			}
 			else {
-				auto clear = got->second.clears.find(sid);
-				if (clear == got->second.clears.end() || clear->second < clearVal)
-					got->second.clears.insert_or_assign(sid, clearVal);
+				auto& clear = got->second.clears[sid];
+				clear = std::max(clear, clearVal);
 			}
 
 			auto get = songTable.find(sid);
@@ -356,9 +355,8 @@ static bool chartReader(const std::string& filename, const std::string& table) {
 				   }
 				   */
 				chart.hcrating = chart.rating;
-				chart.scores = std::vector<std::pair<int, int>>();
 				chart.scores.emplace_back(pid, clearVal);
-				chart.playcount = 1;
+				chart.playcount++;
 				songTable.emplace(sid, chart);
 			}
 			else {

commit 10480f7708ea701d3ae231d5042e11d277d2c15c

fix: initialize variables
Affected &Chart::playcount.
Found by comparing v2 and v1 data.
By the way, v1 has had some duplicate scores.

Fixes: 8aa0496c1ec2 ("refactor: simplify lookup")

goodrating.cpp
@@ -18,8 +18,8 @@
 
 struct Player {
 	std::string name;
-	int lr2id;
-	float rating;
+	int lr2id{};
+	float rating{};
 	std::unordered_map<std::string, int> clears; // md5, clear
 	std::string supplement;
 	// PERF: vector performs better than unordered_map on the amount of levels tables usually have.
@@ -29,11 +29,11 @@ struct Player {
 struct Chart {
 	std::string name;
 	std::unordered_map<std::string, int> tablesFolders;
-	float rating;
-	float hcrating;
+	float rating{};
+	float hcrating{};
 	std::vector<std::pair<int, int>> scores; // [lr2id][clearType]
-	int playcount;
-	float cleardiffsd;
+	int playcount{};
+	float cleardiffsd{};
 };
 
 static constexpr auto&& lr2irplayers = {
@@ -297,7 +297,6 @@ static bool chartReader(const std::string& filename, const std::string& table) {
 				Player player;
 				player.name = playername;
 				player.lr2id = pid;
-				player.rating = 0;
 				player.clears.insert_or_assign(sid, clearVal);
 				playerTable.emplace(pid, player);
 			}